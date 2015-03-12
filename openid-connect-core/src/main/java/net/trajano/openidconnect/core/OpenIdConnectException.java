27a9105062f1917cd4553ed47f8118b486820b07
Archimedes Trajano <arch@trajano.net>
2015-03-12 04:06:05 -0400

    Made it return a JSON

--- openid-connect-core/src/main/java/net/trajano/openidconnect/core/OpenIdConnectException.java
+++ openid-connect-core/src/main/java/net/trajano/openidconnect/core/OpenIdConnectException.java
@@ -3,40 +3,42 @@ package net.trajano.openidconnect.core;
 import java.net.URI;
 
 import javax.ws.rs.BadRequestException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 /**
  * Open ID Connect Exception. This wraps an error response based on the data
  * provided
- * 
+ *
  * @author Archimedes
  */
 public class OpenIdConnectException extends BadRequestException {
 
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = 7363379256768742855L;
 
-    public OpenIdConnectException(ErrorCode error) {
+    public OpenIdConnectException(final ErrorCode error) {
 
         this(new ErrorResponse(error));
     }
 
-    public OpenIdConnectException(ErrorCode error, String errorDescription) {
+    public OpenIdConnectException(final ErrorCode error, final String errorDescription) {
 
         this(new ErrorResponse(error, errorDescription));
     }
 
-    public OpenIdConnectException(ErrorCode error, String errorDescription, URI errorUri) {
+    public OpenIdConnectException(final ErrorCode error, final String errorDescription, final URI errorUri) {
 
         this(new ErrorResponse(error, errorDescription, errorUri));
     }
 
-    public OpenIdConnectException(ErrorResponse errorResponse) {
+    public OpenIdConnectException(final ErrorResponse errorResponse) {
 
         super(Response.ok(errorResponse)
+                .type(MediaType.APPLICATION_JSON)
                 .status(Status.BAD_REQUEST)
                 .build());
     }
